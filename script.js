37697f689ecf2e42f86a7f84ef439b37af7fa0fb
CHRNPS <6206021622178@fitm.kmutnb.ac.th>
2023-10-20 23:10:02 +0900

    mod

--- script.js
+++ script.js
@@ -14,72 +14,143 @@ app.use(BodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 
 
-app.get("/", async (req, res) => {
+app.get("/warehouse", async (req, res) => {
   try {
-      const response = await axios.get(base_url + '/warehouse');
-      res.render("books", { books: response.data });
-  } catch (err) {
-      console.error(err);
-      res.status(500).send('Error');
+    const products = await axios.get(`${base_url}/products`);
+    const companies = await axios.get(`${base_url}/companies`);
+    const productCompanyRelations = await axios.get(`${base_url}/productCompanyRelations`);
+    
+    res.render("warehouse", {
+      products: products.data,
+      companies: companies.data,
+      productCompanyRelations: productCompanyRelations.data
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.get("/book/:id" , async (req, res) => {
+
+app.get("/products", async (req, res) => {
   try {
-      const response = await axios.get(base_url + '/warehouse/' + req.params.id);
-      res.render("book", { book: response.data });
-  } catch (err) {
-      console.error(err);
-      res.status(500).send('Error');
+      const products = await axios.get(`${base_url}/products`);
+      res.render("products/products", { products: products.data });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: error.message });
   }
 });
 
-app.get("/create", (req, res) => {
-  res.render("create");
-});
-
-app.post("/create", async (req, res) => {
+// Edit product page
+app.get("/products/edit/:id", async (req, res) => {
   try {
-      const data = { title: req.body.title, author: req.body.author };
-      await axios.post(base_url + '/warehouse', data);
-      res.redirect("/");
-  } catch (err) {
-      console.error(err);
-      res.status(500).send('Error');
+      const product = await axios.get(`${base_url}/products/${req.params.id}`);
+      res.render("products/editProduct", { product: product.data });
+  } catch (error) {
+      console.error(error);
+      res.status(404).json({ message: error.message });
   }
 });
 
-app.get("/update/:id", async (req, res) => {
+app.post("/products/edit/:id", async (req, res) => {
   try {
-      const response = await axios.get(
-          base_url + '/warehouse/' + req.params.id);
-          res.render("update", {book: response.data});
-      } catch (err) {
-          console.error(err);
-          res.status(500).send('Error');
-      }
-});
+    const productId = req.params.id;
+    const updatedProduct = {
+      ProductName: req.body.productName,
+      StockQuantity: req.body.stockQuantity
+      // Add other properties as needed
+    };
 
-app.post("/update/:id", async (req, res) => {
-  try {
-      const data = { title : req.body.title, author: req.body.author };
-      await axios.put(base_url + '/warehouse/' + req.params.id, data);
-      res.redirect("/");
-  } catch (err) {
-      console.error(err);
-      res.status(500).send('Error');
+    await axios.put(`${base_url}/products/${productId}`, updatedProduct);
+
+    res.redirect(`/products/edit/${productId}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating product" });
   }
 });
 
-app.get("/delete/:id", async (req, res) => {
+
+// Delete product page
+app.get("/products/delete/:id", async (req, res) => {
   try {
-      await axios.delete(base_url + '/warehouse/' + req.params.id);
-      res.redirect("/");
-  } catch (err) {
-      console.error(err);
-      res.status(500).send('Error');
+      await axios.delete(`${base_url}/products/${req.params.id}`);
+      res.redirect("/products");
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: error.message });
   }
 });
+
+// Add new product page
+app.get("/products/add", (req, res) => {
+  res.render("addProduct");
+});
+
+
+app.get("/companies", async (req, res) => {
+  try {
+    const companies = await Company.find(); // Fetch companies from your database
+    res.render("companyPage", { Company: companies });
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Define for create edit, and delete companies
+app.get("/companies", async (req, res) => {
+  try {
+      const companies = await axios.get(`${base_url}/companies`);
+      res.render("companies/companies", { companies: companies.data });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: error.message });
+  }
+});
+// Create Company 
+app.get("/companies/create", (req, res) => {
+  res.render("createCompanyForm");
+});
+
+app.post("/companies/create", async (req, res) => {
+  try {
+    const response = await axios.post(`${base_url}/companies`, req.body);
+    res.redirect("/companies");
+  } catch (error) {
+    res.status(400).send("Bad Request");
+  }
+});
+
+// Edit Company 
+app.get("/companies/edit/:id", async (req, res) => {
+  try {
+    const response = await axios.get(`${base_url}/companies/${req.params.id}`);
+    res.render("editCompanyForm", { Company: response.data });
+  } catch (error) {
+    res.status(404).send("Not Found");
+  }
+});
+
+app.post("/companies/edit/:id", async (req, res) => {
+  try {
+    await axios.put(`${base_url}/companies/${req.params.id}`, req.body);
+    res.redirect("/companies");
+  } catch (error) {
+    res.status(400).send("Bad Request");
+  }
+});
+
+// Delete Company
+app.get("/companies/delete/:id", async (req, res) => {
+  try {
+    await axios.delete(`${base_url}/companies/${req.params.id}`);
+    res.redirect("/companies");
+  } catch (error) {
+    res.status(404).send("Not Found");
+  }
+});
+
 
 app.listen(777, () => {
    console.log('Server started on port 777');
